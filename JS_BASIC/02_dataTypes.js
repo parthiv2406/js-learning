fd4e803065c766b9c7b6d0d6ea45cc407c04eb34
Parthiv Das Mandal <130141458+parthiv2406@users.noreply.github.com>
2024-07-03 15:39:18 +0000

    data types

---
+++ JS_BASIC/02_dataTypes.js
@@ -0,0 +1,10 @@
+let name = "Parthiv"
+let age = 21
+let isStudent= true
+let college;
+let gf =null
+console.table([name,age,isStudent,college])
+console.log(typeof(college))
+// null standalone value
+// undefined=> value does not define
+// typeof(null)==> object
