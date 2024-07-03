9bc0a4b6c748bbeecf3dc4c248bf14848c111434
Parthiv Das Mandal <130141458+parthiv2406@users.noreply.github.com>
2024-07-03 16:31:43 +0000

    conversion

---
+++ JS_BASIC/03_conversion1.js
@@ -0,0 +1,28 @@
+let name = "parthiv"
+console.log(typeof(name))
+let isValueNumber=Number(name)
+console.log(isValueNumber)
+console.log(typeof(isValueNumber))
+
+let isBool= Boolean(name)
+console.log(isBool)
+
+let stringNumber = String(name)
+console.log(typeof(stringNumber))
+console.log(stringNumber)
+
+
+// <<<<------------------ operation------------------->
+// string concatination
+
+let str1="Parthiv"
+let str2= " Das Mondal"
+str3 = str1+str2;
+console.log(str3)
+console.log(+false)
+
+// soime tricky example 
+
+console.log("2"+1-1) // string and opeartion
+console.log("2"+1+1)
+console.log(2+1+"1")
