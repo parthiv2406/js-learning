58a60b6828d80661e673a7339232fad0495d2143
Parthiv Das Mandal <130141458+parthiv2406@users.noreply.github.com>
2024-07-03 12:53:07 +0000

    varible config

---
+++ 01_variable.js
@@ -0,0 +1,17 @@
+let id =40;
+var i = 50;
+const k = 90;
+accountCity="kolkata";
+let idName;
+// not defined varible
+
+// const variable can't be changed..
+
+// let and var are local and global ... var not significant for scope
+
+// non decalre varible 
+
+console.log(accountCity);
+
+// we can print as  a table 
+console.table([id,i,k,accountCity,idName])
